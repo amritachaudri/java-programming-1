9d1e4bf561bd04ff25fb24778f3836fbe976ee1e
Amrita Chaudri <amritachaudri@gmail.com>
2020-08-15 20:31:29 +0530

    question1

---
+++ Calculator-question1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+/*Design a Calculator class which contains four lambda methods:
+
+a. add(a, b)
+b. difference(a, b)
+c. product(a, b)
+d. safeDivision(a, b)
+Also design the main methods which pass actual parameters to test the Calculator class.*/
+//Creating a interface
+interface op
+{
+    int operations(int a,int b);
+}
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        //Creating Lambda methods
+        op addition= (int a, int b)-> a+b;
+        op difference=(int a, int b)-> a-b;
+        op product=(int a, int b)-> a*b;
+        op safeDivision=(int a, int b)-> a/b;
+        //Scanner sc = new Scanner(System.in);
+        // int a= sc.nextInt();
+        //int b= sc.nextInt();
+        System.out.println("1.Addition\n2.Difference\n3.Product\n4.SafeDivision");
+        System.out.println("Enter the option");
+        int n=sc.nextInt();
+        switch (n)
+        {
+            case 1:System.out.println("Addition = "+addition.operations(2,3));
+            break;
+            case 2:System.out.println("Difference = "+difference.operations(5,2));
+            break;
+            case 3:System.out.println("Product = "+product.operations(2,5));
+            break;
+            case 4:System.out.println("SafeDivision = "+safeDivision.operations(10,2));
+            break;
+
+
+        }
+    }
+}
